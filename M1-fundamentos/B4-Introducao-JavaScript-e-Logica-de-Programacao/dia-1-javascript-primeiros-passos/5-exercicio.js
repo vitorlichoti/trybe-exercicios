89f978eb47676c35800c7a8e7a4c1a045a7416f6
vitorlichoti <vitorlichoti@gmail.com>
2022-05-12 15:48:49 -0300

    Adiciona exercicios do dia 1

---
+++ M1-fundamentos/B4-Introducao-JavaScript-e-Logica-de-Programacao/dia-1-javascript-primeiros-passos/5-exercicio.js
@@ -0,0 +1,9 @@
+let weekDay = "quarta-feira";
+const trybeDay = "Oba, mais um dia de aprendizado na Trybe >:D";
+const descanso = "FINALMENTE, descanso merecido UwU";
+
+if (weekDay == "segunda-feira" || weekDay == "terça-feira" || weekDay == "quarta-feira" || weekDay == "quinta-feira" || weekDay == "sexta-feira") {
+    console.log(trybeDay);
+} else {
+    console.log(descanso);
+}
