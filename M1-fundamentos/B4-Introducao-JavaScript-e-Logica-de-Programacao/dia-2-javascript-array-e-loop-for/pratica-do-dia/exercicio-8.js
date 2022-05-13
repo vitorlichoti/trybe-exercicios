759be43a9fcfe68b5fb7ef7f5bd87c737b364459
vitorlichoti <vitorlichoti@gmail.com>
2022-05-13 12:35:20 -0300

    Adiciona Pratica do Dia

---
+++ M1-fundamentos/B4-Introducao-JavaScript-e-Logica-de-Programacao/dia-2-javascript-array-e-loop-for/pratica-do-dia/exercicio-8.js
@@ -0,0 +1,12 @@
+let array = [];
+
+for (let i = 1; i <= 25; i++) {
+    array.push(i);
+}
+
+console.log(array);
+
+// ou entao, imprimindo todos os resultados por linha:
+// for (let num of array) {
+//     console.log(num);
+// }
