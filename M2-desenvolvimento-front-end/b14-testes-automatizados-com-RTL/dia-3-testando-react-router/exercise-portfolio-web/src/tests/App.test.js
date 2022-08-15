acf64d41d569ee12ae7e585cc794cda3856d3ca4
vitorlichoti <vitorlichoti@gmail.com>
2022-08-15 18:28:58 -0300

    Exercicios do dia completo

--- M2-desenvolvimento-front-end/b14-testes-automatizados-com-RTL/dia-3-testando-react-router/exercise-portfolio-web/src/tests/App.test.js
+++ M2-desenvolvimento-front-end/b14-testes-automatizados-com-RTL/dia-3-testando-react-router/exercise-portfolio-web/src/tests/App.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
+const HOME_CONTENT = screen.queryByText('Home Page');
+
 describe('testa se os clicks redirecionam corretamente', () => {
-  it('Testa se o link About é renderizado e redirecionado corretamente', async () => {
+  it('Testa se o link About é renderizado e redirecionado corretamente', () => {
     const { history } = renderWithRouter(<App />);
 
     const linkAbout = screen.getByRole('link', { name: 'Sobre Mim' });
@@ -13,6 +16,7 @@ describe('testa se os clicks redirecionam corretamente', () => {
 
     userEvent.click(linkAbout);
     expect(history.location.pathname).toBe('/about');
+    expect(HOME_CONTENT).not.toBeInTheDocument();
   });
 
   it('Testa se o link Projects é renderizado e redirecionado corretamente', () => {
@@ -24,6 +28,7 @@ describe('testa se os clicks redirecionam corretamente', () => {
     userEvent.click(linkProjetos);
 
     expect(history.location.pathname).toBe('/projects');
+    expect(HOME_CONTENT).not.toBeInTheDocument();
   });
 
   it('Testa se o link Contact é renderizado e redirecionado corretamente', () => {
@@ -35,5 +40,6 @@ describe('testa se os clicks redirecionam corretamente', () => {
     userEvent.click(linkContact);
 
     expect(history.location.pathname).toBe('/contact');
+    expect(HOME_CONTENT).not.toBeInTheDocument();
   });
 });
