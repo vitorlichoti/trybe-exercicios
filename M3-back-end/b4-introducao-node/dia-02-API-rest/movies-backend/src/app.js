3f287e5e925a47a2f511dc58a8e74d6fb9f9e35f
Vitor Lichoti <vitorlichoti@pop-os.localdomain>
2022-10-28 18:52:55 -0400

    exercicios API movies Node/Express

---
+++ M3-back-end/b4-introducao-node/dia-02-API-rest/movies-backend/src/app.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const { writeFile } = require('fs');
+
+const fs = require('fs').promises;
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+
+const moviesPath = path.resolve(__dirname, './movies.json');
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(moviesPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Arquivo nao pode ser lido: ${error}`)
+  }
+};
+
+app.get('/movies/:id', async (req, res) => {
+  try {
+    const movies = await readFile();
+    const movie = movies.find(({id}) => id === Number(req.params.id));
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).send({ message: error.message})
+  }
+});
+
+app.get('/movies', async (req, res) => {
+  try {
+    const movies = await readFile();
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.post('/movies', async (req, res) => {
+  try {
+    const movies = readFile();
+    const { movie, price } = req.body;
+    const newMovie = {
+      id: movies[movies.length - 1].id + 1,
+      movie,
+      price,
+    }
+    const allMovies = JSON.stringify([... movies, newMovie]);
+    await fs.writeFile(moviesPath, allMovies);
+    res.status(201).json(newMovie);
+  } catch (error) {
+    res.status(500).send({ message: error.message })
+  }
+});
+
+app.put('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { movie, price } = req.body;
+    const movies = await readFile();
+    const index = movies.findIndex((e) => e.id === Number(id));
+    movies[index] = {
+      id: Number(id),
+      movie,
+      price,
+    }
+    const updateMovies = JSON.stringify(movies, null, 2);
+    await fs.writeFile(moviesPath, updateMovies);
+    res.status(200).json(movies[index]);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.delete('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movies = writeFile();
+    const filteredMovie = movies.filter((e) => e.id !== Number(id));
+    const updateMovies = JSON.stringify(filteredMovie, null, 2);
+    await fs.writeFile(moviesPath, updateMovies);
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+// bonuss
+app.get('/movies/seach', async (req, res) => {
+  try { 
+    const { q } = req.query;
+    const movies = await readFile();
+    
+    if(q) {
+      const filteredMovies = movies.filter((e) => e.movie.includes(q));
+      return res.status(200).json(filteredMovies);
+    }
+    res.status(200).end();
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+module.exports = app;
