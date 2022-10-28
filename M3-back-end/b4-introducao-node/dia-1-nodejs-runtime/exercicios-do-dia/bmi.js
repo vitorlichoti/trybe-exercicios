25c9cc73b4378b2659941c94167e241bfd50ae82
Vitor Lichoti <vitorlichoti@pop-os.localdomain>
2022-10-28 13:58:01 -0400

    Adiciona exercicios do dia, node modules

---
+++ M3-back-end/b4-introducao-node/dia-1-nodejs-runtime/exercicios-do-dia/bmi.js
@@ -0,0 +1,28 @@
+// Exercicio 1
+// const weight = 80;
+// const height = 1.75;
+
+// const bmiCalc = (weight, height) => weight / (height * height);
+
+// console.log(bmiCalc(weight, height));
+
+//Exercicio 2 -> foi colocar o "bmi": "./bmi.js"
+
+// Exercicio 3 e 4 e 5
+// const readlineSync = require('readline-sync');
+
+// const bmiCalc = (weight, height) => weight / (height * height);
+
+// const engine = () => {
+  
+//   const peso = readlineSync.questionFloat('qual é o seu peso? ');
+//   const altura = readlineSync.questionFloat('qual é a sua altura? ');
+//   const result = bmiCalc(peso, altura);
+  
+//   console.log(`O seu IMC é ${result.toPrecision(3)}`);
+  
+// };
+
+// engine();
+
+// Exercicio 6
