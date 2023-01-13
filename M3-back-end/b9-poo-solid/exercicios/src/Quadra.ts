5c42955a2be8aff576ef81d31eb87bfcdcb398b1
Vitor Lichoti <dev.itor@pop-os.localdomain>
2023-01-13 19:30:07 -0400

    Exercicio 5

--- M3-back-end/b9-poo-solid/exercicios/src/Quadra.ts
+++ M3-back-end/b9-poo-solid/exercicios/src/Quadra.ts
@@ -1,5 +1,7 @@
+import IAgenda from './interfaces/IAgenda';
+
 abstract class Quadra {
-  protected abstract reservar<T>(reserva: Date)
+  protected abstract reservar<T>(horaReserva: Date): IAgenda<T>;
 }
 
 export default Quadra;
